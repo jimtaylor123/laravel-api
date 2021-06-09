8d8805b64029921e481fcdc45c5f0d300f23ecfc
Jim Taylor <jim@jimtaylor.space>
2021-06-09 01:02:49 +0100

    WIP

--- app/Http/Controllers/v1/User/UserProjectController.php
+++ app/Http/Controllers/v1/User/UserProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\v1\User\Projects;
 use App\Models\User;
 use App\Services\JSONAPIService;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\JSONAPIRelationshipRequest;
 
 class UserProjectController extends Controller
 {
@@ -21,5 +22,15 @@ class UserProjectController extends Controller
     public function index(User $user)
     {
         return $this->service->fetchRelated($user, 'projects');
+    }
+
+    public function showRelationship(User $user)
+    {
+        return $this->service->fetchRelationship($user, 'projects');
+    }
+
+    public function updateRelationship(JSONAPIRelationshipRequest $request, User $user)
+    {
+        return $this->service->updateToManyRelationships($user, 'projects', $request->input('data.*.id'));
     }
 }
